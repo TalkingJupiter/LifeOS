db1d9f10f93b8b36eb2407595ade0919a2560489
TalkingJupiter <dev.batuhansencer@gmail.com>
2026-01-03 22:09:26 -0600

    Initial Commit

---
+++ LifeOS/LifeOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeOSApp.swift
+//  LifeOS
+//
+//  Created by Batuhan Sencer on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct LifeOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
